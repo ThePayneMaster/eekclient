292fe89f22f09e8ddf91736bf96528831f7beb10
ThePayneMaster <77413826+ThePayneMaster@users.noreply.github.com>
2021-02-06 18:24:44 +0100

    Update eek.bat

--- files/installer/test/eek.bat
+++ files/installer/test/eek.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ThePayneMaster/eekclient/raw/main/files/installer/test/havookBETA.jar', 'havook.jar')"
-powershell -Command "Invoke-WebRequest https://github.com/ThePayneMaster/eekclient/raw/main/files/installer/test/havookBETA.jar -OutFile havook.jar"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/ThePayneMaster/eekclient/raw/main/files/installer/test/havookBETA.jar', '%appdata%\.minecraft\mods\eek_v0.0.1-1-12-2.jar')"
+powershell -Command "Invoke-WebRequest https://github.com/ThePayneMaster/eekclient/raw/main/files/installer/test/havookBETA.jar -OutFile %appdata%\.minecraft\mods\eek_v0.0.1-1-12-2.jar"
